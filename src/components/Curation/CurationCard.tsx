b103c367261f06645d6aee2efc2c0ca879342945
Juhye-Kim <kjh960215@gmail.com>
2021-04-11 02:33:15 +0900

    resolved #63

--- src/components/Curation/CurationCard.tsx
+++ src/components/Curation/CurationCard.tsx
@@ -1,7 +1,51 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-function CurationCard() {
-  return <div></div>;
+interface CurationCardProps {
+  props: {
+    curationCardId: number;
+    theme: number;
+    title: string;
+    detail: string;
+    photo: string;
+    avgTime: number;
+    feedbackCnt: number;
+  };
 }
+
+const CurationCard = ({ props }: CurationCardProps) => {
+  const {
+    curationCardId,
+    theme,
+    title,
+    detail,
+    photo,
+    avgTime,
+    feedbackCnt,
+  } = props;
+  const themeList = ["🍽", "☕️", "🕹", "🚴🏻", "🚗", "🤔"];
+  const history = useHistory();
+  const handleClickCurationCard = () => {
+    history.push(`/viewcuration/${curationCardId}`);
+  };
+  return (
+    <li className="curation-card" onClick={handleClickCurationCard}>
+      <div className="curation-card__info">
+        <div className="curation-card__info__theme">
+          <span>{`${themeList[theme]}`}</span>
+        </div>
+        <div className="curation-card__info__desc">
+          <span className="curation-card__info__desc-title">{title}</span>
+          <div className="curation-card__info__desc-summary">
+            <span>{`⏱ ${avgTime}H`}</span>
+            <span>{`피드백 ${feedbackCnt}개`}</span>
+          </div>
+        </div>
+      </div>
+      <div className="curation-card__add-btn">✚</div>
+      <div className="curation-card__add-btn-desc">내일정에 추가</div>
+    </li>
+  );
+};
 
 export default CurationCard;
